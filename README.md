41683a28bf5f57a0fe387294d62fb6cfd5f06dec
Angelo Burzi <angelo.burzi@immobiliare.it>
2022-11-28 19:23:54 +0100

    Created

---
+++ README.md
@@ -0,0 +1,97 @@
+# microtunnel-client
+
+**microtunnel-client** is an [axios](https://www.npmjs.com/package/axios) based library to enable post-quantum protected communication between apps (it must be used with microtunnel-server). You can send any JSON-serializable data. It also uses [supersphincs](https://www.npmjs.com/package/supersphincs) for app authentication, [kyber-crystals](https://www.npmjs.com/package/kyber-crystals) for asymmetric encryption and [symcryptor](https://www.npmjs.com/package/symcryptor) for symmetric encryption.
+
+## Installation
+
+```bash
+npm i microtunnel-client
+```
+
+## Usage
+
+First, you have to create app credentials and save them in a json file. Enter in module path typing `cd path-to-your-project/node-modules/microtunnel-client` and then run `npm run -s cred-generate > ../../appCred.json` to save the credentials in your project root.
+
+```javascript
+// appCred.json
+{
+    // Supersphincs keys base-64 encoded
+    "publicKey": "rPyoqSZrNNUVpjKdhGLDD4sjXd8lgIgnRBY2NP5n8PDDLSvoLoD5n4GjaxbAfSDjagBjN8zztUQTNG1EKO9IgpgTLkfkTkhWqdgkC/K3EQLh6AMCZ8snlnles2QrbHAy",
+    "privateKey": "FKQ243eKoZ1zAdfXfGkzjcONsFwXbb2YliwoFSMFjNSs/KipJms01RWmMp2EYsMPiyNd3yWAiCdEFjY0/mfw8MkQY9Orp5MsbUAf54jM1iUhvEhaUJceqTG92ibMdu914IaJsN5+3hKHhAZ1o+dtspIY09zuZKNe48hEjlLwjg3DLSvoLoD5n4GjaxbAfSDjagBjN8zztUQTNG1EKO9IgpgTLkfkTkhWqdgkC/K3EQLh6AMCZ8snlnles2QrbHAy",
+
+    // Random bytes base-64 encoded for ID
+    "agent": "vwoA1JzkT6d7SXjIBoZ2egYlSn6Ajzge"
+}
+```
+
+Then you have to create a JSON file containing servers info. In this example we have two apps named `backEnd` and `sessions`:
+
+```javascript
+// authServers.json
+{
+    "backEnd": {
+        "url": "http://127.0.0.1:3000",
+        "publicKey": "X67kzs9zrKfbayvF5SIsulZzfUYHeTm6BoFTD/BWiryIcOWcaR8d6M4LpaOylCi4DqY59ABNt1nNnfFZjG4akE4hcKaMyx5ar9Uds2Op687uecLGWb0n6W+voSDKzMS8",
+        "agent": "285gWsTqj3Gza+3AxJn1qrWzAvf/Lf5i"
+    },
+    "sessions": {
+        "url": "http://192.168.0.3:3000",
+        "publicKey": "rPyoqSZrNNUVpjKdhGLDD4sjXd8lgIgnRBY2NP5n8PDDLSvoLoD5n4GjaxbAfSDjagBjN8zztUQTNG1EKO9IgpgTLkfkTkhWqdgkC/K3EQLh6AMCZ8snlnles2QrbHAy",
+        "agent": "vwoA1JzkT6d7SXjIBoZ2egYlSn6Ajzge"
+    }
+}
+```
+
+Then you can use it in your code:
+
+```javascript
+const authReq = require( 'microtunnel-client' )( { appCredFile: 'appCred.json', authServersFile: 'authServers.json' } );
+( async () => {
+    try {
+        let resp = await authReq.get( 'backEnd', '/page/2' );
+        console.log( resp );
+        resp = await authReq.post( 'sessions', '/another-route', { data: 'mydata' } );
+        console.log( resp );
+    } catch ( err ) {
+        console.log( err );
+    }
+} )();
+```
+
+## Configuration
+
+### `require( 'microtunnel-client' )( options )`
+
+#### `options`
+
+* `api` Optional - Root path for microtunnel (note: must be the same for servers) - Default `'/microtunnel'`
+* `timeout` Optional - Axios timeout for each request
+* `appCredFile` Optional - Relative path of the credentials file - Default: enviroment var `APP_CRED`
+* `authServersFile` Optional - Relative path of the autherized clients file - Default: enviroment var `AUTH_SRVS`
+
+## Methods
+
+### `authReq.get`
+```javascript
+authReq.get( serverName: String, serverPath: String )
+```
+
+#### Parameters
+* `serverName` Required - The name of the server as written in authServers.json
+* `serverPath` Required - The path of the server you want to fetch
+
+#### Return
+Decrypted JSON parsed data from server when `Promise` resolved else throw an `Error`
+
+### `authReq.post`
+```javascript
+authReq.post( serverName: String, serverPath: String, obj )
+```
+
+#### Parameters
+* `serverName` Required - The name of the server as written in authServers.json
+* `serverPath` Required - The path of the server you want to fetch
+* `obj` Required - The data you want to send. It must be JSON serializable
+
+#### Return
+Decrypted JSON parsed data from server when `Promise` resolved else throw an `Error`
---
+++ index.js
@@ -0,0 +1,244 @@
+"use strict";
+const superSphincs = require( 'supersphincs' ),
+    kyber = require( 'kyber-crystals' ),
+    symCryptor = require( 'symcryptor' );
+if ( process.argv[1] === __filename && process.argv[2] === 'cred-generate' ) {
+    Promise.all( [
+        superSphincs.keyPair(),
+        symCryptor.rndBytes( 24 )
+    ] )
+    .then( res => {
+        const cred = {
+            publicKey: Buffer.from( res[0].publicKey ).toString( 'base64' ),
+            privateKey: Buffer.from( res[0].privateKey ).toString( 'base64' ),
+            agent: res[1].toString( 'base64' )
+        }
+        console.log( JSON.stringify( cred, null, 4 ) );
+    } )
+    .catch( console.error )
+    .finally( process.exit );
+} else {
+    const create = ( options = {} ) => {
+        const defaultOptions = {
+            api: '/microtunnel',
+            timeout: 5000,
+            appCredFile: process.env.APP_CRED,
+            authServersFile: process.env.AUTH_SRVS,
+            ...options
+        };
+        const { api, timeout, appCredFile, authServersFile } = defaultOptions;
+        const 
+            appCred = require( './' + appCredFile ),
+            authServers = require( './' + authServersFile ),
+            HttpAgent = require( 'agentkeepalive' ),
+            { HttpsAgent } = HttpAgent,
+            client = require( 'axios' ).create( {
+                timeout,
+                httpAgent: new HttpAgent(),
+                httpsAgent: new HttpsAgent(),
+                maxRedirects: 0,
+                responseType: 'arraybuffer',
+                headers: {
+                    "User-Agent": appCred.agent,
+                    "Connection": 'keep-alive'
+                }
+            } ),
+            postOptions = {
+                headers: {
+                    'Content-Type': 'application/octet-stream'
+                }
+            }
+        ;
+        class AuthClientSex {
+            constructor( name, url, agent, signature ) {
+                this.name = name;
+                this.url = url;
+                this.agent = agent;
+                this.signature = signature;
+                this.active = false;
+                this.key = undefined;
+                this.shaKey = undefined;
+                this.connecting = false;
+            }
+            delete() {
+                this.active = false;
+                this.key = undefined;
+                this.shaKey = undefined;
+            }
+            extract() {
+                const obj = {
+                    name: this.name,
+                    url: this.url,
+                    agent: this.agent,
+                    signature: this.signature,
+                    active: this.active,
+                    key: this.key,
+                    shaKey: this.shaKey
+                };
+                return obj;
+            }
+            reset() {
+                this.delete();
+            }
+            async connect() {
+                try {
+                    this.connecting = true;
+                    let body;
+                    const rndStr = await symCryptor.rndBytes( 64 );
+                    try {
+                        body = new Uint8Array( ( await client.post( this.url + api +'/auth1', rndStr, postOptions ) ).data );
+                    } catch ( err ) {
+                        throw new Error( 'Internal server error with server ' + this.name );
+                    }
+                    if (
+                        body.length !== 31490
+                    ) throw new Error( 'Internal server error with server ' + this.name );
+                    const very = await superSphincs.verifyDetached( body.slice( 1568, -64 ), rndStr, this.signature, this.agent );
+                    if ( !very ) throw new Error( 'Internal server error with server ' + this.name );
+                    const pubKey = body.slice( 0, 1568 );
+                    const strToSign = body.slice( -64 );
+                    const { cyphertext, secret } = await kyber.encrypt( pubKey );
+                    this.key = secret;
+                    this.shaKey = await symCryptor.rndBytes( 32 );
+                    const signed = await superSphincs.signDetached( strToSign, Buffer.from( appCred.privateKey, 'base64' ), this.shaKey );
+                    const pst = Buffer.concat( [
+                        cyphertext,
+                        await symCryptor.encrypt( this.shaKey, this.key ),
+                        await symCryptor.encrypt( signed, this.key, this.shaKey, appCred.agent )
+                    ] );
+                    body = ( await client.post( this.url + api + '/auth2', pst, postOptions ) ).data;
+                    if ( body.length !== 84 ) throw new Error( 'Internal server error with server ' + this.name );
+                    const confirmation = await symCryptor.decrypt( body, this.key, this.shaKey, this.agent );
+                    if ( confirmation.toString() === 'true' ) {
+                        this.active = true;
+                    }
+                } catch {
+                    throw new Error( 'Internal server error with server ' + this.name );
+                } finally {
+                    this.connecting = false;
+                }
+                
+            }
+        }
+    
+        class AuthClientSessions extends Array {
+            constructor( authServers ) {
+                const servs = [];
+                for ( const srv in authServers ) {
+                    servs.push( new AuthClientSex( srv, authServers[srv].url, authServers[srv].agent, Buffer.from( authServers[srv].publicKey, 'base64' ) ) );
+                }
+                super( ...servs );
+            }
+            get( str ) {
+                if ( !str || typeof str !== 'string' ) return false;
+                const found = this.find( el => el.name === str );
+                if ( found ) return found;
+                return false;
+            }
+            extract( str ) {
+                const found = this.get( str );
+                if ( !found ) return found;
+                return found.extract();
+            }
+            reset( str ) {
+                const found = this.get( str );
+                if ( !found ) return found;
+                return found.reset();
+            }
+        }
+    
+        const sessions = new AuthClientSessions( authServers );
+    
+        const srvReq = {
+            async get( serverName, serverPath = '/' ) {
+                const srv = sessions.get( serverName );
+                while ( srv.connecting ) await new Promise( r => setTimeout( r, 1000 ) );
+                if ( !srv.active ) {
+                    try {
+                        await srv.connect();
+                    } catch {
+                        await new Promise( r => setTimeout( r, 2000 ) );
+                        await srv.connect();
+                    }
+                }
+                let body;
+                try {
+                    body = ( await client.get( srv.url + api + serverPath ) ).data;
+                } catch {
+                    try {
+                        srv.delete();
+                        while ( srv.connecting ) await new Promise( r => setTimeout( r, 1000 ) );
+                        try {
+                            await srv.connect();
+                        } catch {
+                            await new Promise( r => setTimeout( r, 2000 ) );
+                            await srv.connect();
+                        }
+                        body = ( await client.post(
+                            srv.url + api + serverPath,
+                            await symCryptor.encrypt( Buffer.from( JSON.stringify( obj ) ), srv.key, srv.shaKey, appCred.agent ),
+                            postOptions
+                        ) ).data;
+                    } catch ( err ) {
+                        throw err;
+                    }
+                }
+                try {
+                    return JSON.parse( await symCryptor.decrypt( body, srv.key, srv.shaKey, srv.agent ) );
+                } catch {
+                    throw new Error( 'Internal server error with server ' + serverName );
+                }
+            },
+            async post( serverName, serverPath = '/', obj ) {
+                const srv = sessions.get( serverName );
+                while ( srv.connecting ) await new Promise( r => setTimeout( r, 1000 ) );
+                if ( !srv.active ) {
+                    try {
+                        await srv.connect();
+                    } catch {
+                        await new Promise( r => setTimeout( r, 2000 ) );
+                        await srv.connect();
+                    }
+                }
+                serverPath = serverPath || '/';
+                let body
+                try {
+                    body = ( await client.post(
+                        srv.url + api + serverPath,
+                        await symCryptor.encrypt( Buffer.from( JSON.stringify( obj ) ), srv.key, srv.shaKey, appCred.agent ),
+                        postOptions
+                    ) ).data;
+                } catch {
+                    try {
+                        srv.delete();
+                        while ( srv.connecting ) await new Promise( r => setTimeout( r, 1000 ) );
+                        try {
+                            await srv.connect();
+                        } catch {
+                            await new Promise( r => setTimeout( r, 2000 ) );
+                            await srv.connect();
+                        }
+                        body = ( await client.post(
+                            srv.url + api + serverPath,
+                            await symCryptor.encrypt( Buffer.from( JSON.stringify( obj ) ), srv.key, srv.shaKey, appCred.agent ),
+                            postOptions
+                        ) ).data;
+                    } catch ( err ) {
+                        throw err;
+                    }
+                }
+                try {
+                    const encoded = JSON.parse( await symCryptor.decrypt( body, srv.key, srv.shaKey, srv.agent ) );
+                    return encoded;
+                } catch {
+                    throw new Error( 'Internal server error with server ' + serverName );
+                }
+            },
+            symCryptor
+        };
+        return srvReq;
+    };
+    
+    
+    module.exports = create;
+}
